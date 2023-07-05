246310b9f0acd794430036154aaae6c21b87b3ed
desaidhruv <dhruv.desai@kevit.io>
2023-07-05 18:07:22 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Style/App.css";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [completedItems, setCompletedItems] = useState([]);
+
+  const inputChange = (event) => {
+    setItem(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (item.trim() !== "") {
+      setTodo((prevItem) => [...prevItem, item]);
+      setItem("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    setTodo((prevItem) => {
+      const updatedItems = [...prevItem];
+      updatedItems.splice(index, 1);
+      return updatedItems;
+    });
+  };
+
+  const handleCheckboxChange = (index) => {
+    const selectedTodo = todo[index];
+    setCompletedItems((prevCompletedItems) => [
+      ...prevCompletedItems,
+      selectedTodo,
+    ]);
+    handleDelete(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="searchConatiner">
+        <input
+          type="text"
+          onChange={inputChange}
+          value={item}
+          placeholder="Enter Item"
+          className="inputSearch"
+        />
+        <button onClick={handleSubmit} className="btn">
+          Submit
+        </button>
+      </div>
+      <div className="listConatiner">
+        <ul>
+          {todo.map((listItems, index) => (
+            <li key={index}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheckboxChange(index)}
+              />
+              <h3>{listItems}</h3>
+              <button onClick={() => handleDelete(index)} className="btn">
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {completedItems.length > 0 && <hr />}
+      <div className="checkedItem">
+        <ul>
+          <h2>Listed Item</h2>
+          {completedItems.map((completedItem, index) => (
+            <li key={index}>
+              <input type="checkbox" checked={true} />
+              <h3>{completedItem}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
